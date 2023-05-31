0bcea2ca16ee142b424b276870377e9b86aaf596
Alisher667 <alisher7@me.com>
2023-05-31 15:16:23 +0600

    Update NetworkInspectorRequestBodyView.swift

--- Sources/PulseUI/Features/Inspector/NetworkInspectorRequestBodyView.swift
+++ Sources/PulseUI/Features/Inspector/NetworkInspectorRequestBodyView.swift
@@ -39,13 +39,11 @@ struct NetworkInspectorRequestBodyView: View {
 
 final class NetworkInspectorRequestBodyViewModel {
     private(set) lazy var fileViewModel = data.map { data in
-        if #available(iOS 14.0, *) {
         FileViewerViewModel(
             title: "Request Body",
             context: task.requestFileViewerContext,
             data: { data }
         )
-        }
     }
 
     private var data: Data? {
