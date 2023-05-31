89f55fd84938af203543cd89f935f9f65a740532
Alisher667 <alisher7@me.com>
2023-05-31 15:10:37 +0600

    Update ConsoleView.swift

--- Sources/PulseUI/Features/Console/ConsoleView.swift
+++ Sources/PulseUI/Features/Console/ConsoleView.swift
@@ -7,6 +7,7 @@ import CoreData
 import Pulse
 import Combine
 
+@available(iOS 14.0, *)
 extension ConsoleView {
     public init(store: LoggerStore = .shared, mode: ConsoleMode = .all) {
         self.init(environment: .init(store: store, mode: mode))
